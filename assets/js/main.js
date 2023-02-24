8a6db39ac279ae779c184e0d66f8b40c59c93420
Niko Kacic <niko.kacic@gmail.com>
2023-02-24 19:09:26 -0300

    Función updateToDos agregada para agregar los elementos al local storage creada & transformarlas a texto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 
-const to_dos = [];//arreglo de <li> a agregar
+/*Se alamcenaran las listas en local storage y se agrega un short circuit para el arregl vacio no devuelva null*/
+const to_dos = JSON.parse(localStorage.getItem('to_dos')) || [];//arreglo de <li> a agregar
 
 const form = document.getElementById('to-do-form');
+
 
 const render = () => {
     const to_do_list = document.getElementById('to-do-list');//se captura la lista
     
-    const to_dos_template = to_dos.map(t =>  `<li>${t}</li>` );//con map se devuelve el template de li
+    const to_dos_template = to_dos.map(t =>  '<li>'+t+'</li>' );//con map se devuelve el template de li
  
     to_do_list.innerHTML = to_dos_template.join('');//se agrega html y se les hace join(con strings);
  
@@ -15,18 +17,28 @@ const render = () => {
         elemento.addEventListener('click', ()=> {
             elemento.parentNode.removeChild(elemento);//con parentNode que es el nodo padre tiene capacidad eliminar elementos hijos con el metodo removeChild(elemento a borrar);
             to_dos.splice(index, 1);
+            updateToDos(to_dos)
             render()//se renderiza la lista para que se actualice la lista de los array con recursividad
         })
     })
 }
+
+render();
+
+
+const updateToDos = (to_dos) => {
+    const to_dos_strings = JSON.stringify(to_dos);
+    localStorage.setItem('to_dos', to_dos_strings);
+}
+
+
 
 form.onsubmit = (e) => {
     e.preventDefault();//detiene comportamiento formulario el refrescar la pagina
     const to_do = document.getElementById('to-do');//captura del input y su valor
     const to_do_text = to_do.value;
     to_do.value = '';//una vez ingresado, remplazar por un vacio
-    
     to_dos.push(to_do_text);//agregando <li> al arreglo vacio
-
+    updateToDos(to_dos);
     render(); 
 }
